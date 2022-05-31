d55f57802ca1885a3cbc46c92750e58778bbac42
darthinvader <psaradakisj@gmail.com>
2022-05-31 16:24:06 +0200

    [learning] describe expect

---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import TodoFooter from "../TodoFooter";
+
+const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  );
+};
+
+describe("TodoFooter", () => {
+  it("should render the correct ammount of incomplete tasks", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it("should render the correct ammount of incomplete task", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it("should render the correct ammount of incomplete task new assertion", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toHaveTextContent("1 task left");
+  });
+
+  it("should render the correct ammount of incomplete task new assertion value", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement.textContent).toBe("1 task left");
+  });
+});
